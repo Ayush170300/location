a4ce2d2ab9ceaed91b8a8718efb31617d667ee31
Ayush170300 <ayushparihar1703@gmail.com>
2021-04-04 12:37:52 +0530

    4th commit

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -19,13 +19,14 @@ const Forecast = ({lat,long}) => {
     useEffect(() => {
         const getForecast=async()=>{
             const {data}=await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=82a4674269ac8f84ea2f4e73ddb3f99e&units=metric`)
-            setWeather1(String((data.list[0].main.temp).toFixed(0))+"/"+String((data.list[2].main.temp).toFixed(0)))
-            setWeather2(String((data.list[3].main.temp).toFixed(0))+"/"+String((data.list[10].main.temp).toFixed(0)))
-            setWeather3(String((data.list[11].main.temp).toFixed(0))+"/"+String((data.list[18].main.temp).toFixed(0)))
-            setIcon([data.list[0].weather[0].icon,data.list[3].weather[0].icon,data.list[11].weather[0].icon])
-            setDesc([data.list[0].weather[0].description,data.list[3].weather[0].description,data.list[11].weather[0].description])
-            setdate([data.list[0].dt_txt,data.list[3].dt_txt,data.list[11].dt_txt])
+            setWeather1(String((data.list[8].main.temp).toFixed(0))+"/"+String((data.list[9].main.temp).toFixed(0)))
+            setWeather2(String((data.list[16].main.temp).toFixed(0))+"/"+String((data.list[17].main.temp).toFixed(0)))
+            setWeather3(String((data.list[24].main.temp).toFixed(0))+"/"+String((data.list[25].main.temp).toFixed(0)))
+            setIcon([data.list[8].weather[0].icon,data.list[16].weather[0].icon,data.list[24].weather[0].icon])
+            setDesc([data.list[8].weather[0].description,data.list[16].weather[0].description,data.list[24].weather[0].description])
+            setdate([data.list[8].dt_txt,data.list[16].dt_txt,data.list[24].dt_txt])
             // console.log(icon[0])
+            console.log(data)
         
             
         }
@@ -33,17 +34,17 @@ const Forecast = ({lat,long}) => {
     },[lat,long])
     return (
         <div>
-            <Row>
+            <Row >
                 <Col md={4}><p style={{fontSize:"14px"}}>{date[0]}</p></Col>
                 <Col md={5}><p style={{fontSize:"14px"}}><img  src={"https://openweathermap.org/img/w/"+icon[0]+".png"} alt=""></img>{weather1}&#8451;</p></Col>
                 <Col md={3}><p style={{fontSize:"12px",lineHeight:"45px"}}>{desc[0]}</p></Col>
             </Row>
-            <Row>
+            <Row >
                 <Col  md={4} ><p style={{fontSize:"14px"}}>{date[1]}</p></Col>
                 <Col md={5} ><p style={{fontSize:"14px"}}><img  src={"https://openweathermap.org/img/w/"+icon[1]+".png"} alt=""></img>{weather2}&#8451;</p></Col>
                 <Col md={3}><p style={{fontSize:"12px",lineHeight:"40px"}}>{desc[1]}</p></Col>
             </Row>
-            <Row>
+            <Row >
                 <Col  md={4} ><p style={{fontSize:"14px"}}>{date[2]}</p></Col>
                 <Col md={5}><p style={{fontSize:"14px"}}><img  src={"https://openweathermap.org/img/w/"+icon[2]+".png"} alt=""></img>{weather3}&#8451;</p></Col>
                 <Col md={3}><p style={{fontSize:"12px",lineHeight:"40px"}}>{desc[2]}</p></Col>
